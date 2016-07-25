58dc68ef6618e348f4fbf875735176f05f02d1d0
Suket Shah - Network <C228687@ema.LILLY.com>
2016-07-25 15:35:44 +0530

    new file added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718140923) do
+ActiveRecord::Schema.define(version: 20160718083713) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
